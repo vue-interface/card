9312d056d1967d1622b63f17699cdbddf1ab2604
Mitchell Solomon <msolomon@actengage.com>
2022-09-22 15:02:28 -0400

    Vue 3

--- dist/card.es.js
+++ dist/card.es.js
@@ -1,72 +1,37 @@
-var render = function() {
-  var _vm = this;
-  var _h = _vm.$createElement;
-  var _c = _vm._self._c || _h;
-  return _c("div", { staticClass: "card" }, [_vm.$slots["img-top"] ? [_vm._t("img-top")] : _vm._e(), _vm.$slots.header ? [_c("div", { staticClass: "card-header" }, [_vm._t("header")], 2)] : _vm._e(), _vm._t("default", function() {
-    return [_vm.$slots.body ? [_c("div", { staticClass: "card-body" }, [_vm._t("body")], 2)] : _vm._e()];
-  }), _vm.$slots.footer ? [_c("div", { staticClass: "card-footer" }, [_vm._t("footer")], 2)] : _vm._e(), _vm.$slots["img-bottom"] ? [_vm._t("img-bottom")] : _vm._e()], 2);
+import { openBlock as t, createElementBlock as r, renderSlot as e, createCommentVNode as s } from "vue";
+const c = (o, d) => {
+  const l = o.__vccOpts || o;
+  for (const [n, a] of d)
+    l[n] = a;
+  return l;
+}, i = {}, m = { class: "card" }, $ = {
+  key: 1,
+  class: "card-header"
+}, f = {
+  key: 0,
+  class: "card-body"
+}, u = {
+  key: 2,
+  class: "card-footer"
 };
-var staticRenderFns = [];
-function normalizeComponent(scriptExports, render2, staticRenderFns2, functionalTemplate, injectStyles, scopeId, moduleIdentifier, shadowMode) {
-  var options = typeof scriptExports === "function" ? scriptExports.options : scriptExports;
-  if (render2) {
-    options.render = render2;
-    options.staticRenderFns = staticRenderFns2;
-    options._compiled = true;
-  }
-  if (functionalTemplate) {
-    options.functional = true;
-  }
-  if (scopeId) {
-    options._scopeId = "data-v-" + scopeId;
-  }
-  var hook;
-  if (moduleIdentifier) {
-    hook = function(context) {
-      context = context || this.$vnode && this.$vnode.ssrContext || this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext;
-      if (!context && typeof __VUE_SSR_CONTEXT__ !== "undefined") {
-        context = __VUE_SSR_CONTEXT__;
-      }
-      if (injectStyles) {
-        injectStyles.call(this, context);
-      }
-      if (context && context._registeredComponents) {
-        context._registeredComponents.add(moduleIdentifier);
-      }
-    };
-    options._ssrRegister = hook;
-  } else if (injectStyles) {
-    hook = shadowMode ? function() {
-      injectStyles.call(this, (options.functional ? this.parent : this).$root.$options.shadowRoot);
-    } : injectStyles;
-  }
-  if (hook) {
-    if (options.functional) {
-      options._injectStyles = hook;
-      var originalRender = options.render;
-      options.render = function renderWithStyleInjection(h, context) {
-        hook.call(context);
-        return originalRender(h, context);
-      };
-    } else {
-      var existing = options.beforeCreate;
-      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
-    }
-  }
-  return {
-    exports: scriptExports,
-    options
-  };
+function y(o, d) {
+  return t(), r("div", m, [
+    o.$slots["img-top"] ? e(o.$slots, "img-top", { key: 0 }) : s("", !0),
+    o.$slots.header ? (t(), r("div", $, [
+      e(o.$slots, "header")
+    ])) : s("", !0),
+    e(o.$slots, "default", {}, () => [
+      o.$slots.body ? (t(), r("div", f, [
+        e(o.$slots, "body")
+      ])) : s("", !0)
+    ]),
+    o.$slots.footer ? (t(), r("div", u, [
+      e(o.$slots, "footer")
+    ])) : s("", !0),
+    o.$slots["img-bottom"] ? e(o.$slots, "img-bottom", { key: 3 }) : s("", !0)
+  ]);
 }
-const __vue2_script = {};
-const __cssModules = {};
-var __component__ = /* @__PURE__ */ normalizeComponent(__vue2_script, render, staticRenderFns, false, __vue2_injectStyles, null, null, null);
-function __vue2_injectStyles(context) {
-  for (let o in __cssModules) {
-    this[o] = __cssModules[o];
-  }
-}
-var Card = /* @__PURE__ */ function() {
-  return __component__.exports;
-}();
-export { Card };
+const k = /* @__PURE__ */ c(i, [["render", y]]);
+export {
+  k as Card
+};
