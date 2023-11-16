26e30c25f597650f909508e511c6bcea0efdb6be
Justin Kimbrell <jkimbrell@actengag.com>
2023-11-16 09:58:27 -0700

    fix: added dark mode support

--- src/Card.vue
+++ src/Card.vue
@@ -24,7 +24,7 @@ withDefaults(defineProps<{
         </div>
         <slot name="body">
             <div
-                class="flex-1 border border-stone-200"
+                class="flex-1 border border-stone-200 dark:border-stone-700"
                 :class="{
                     'p-3': size === 'sm',
                     'p-4': size === 'md',
@@ -39,7 +39,7 @@ withDefaults(defineProps<{
         </slot>
         <div
             v-if="$slots.footer"
-            class="flex items-center justify-end gap-4 border border-t-0 border-stone-200 px-6 py-4 text-sm"
+            class="flex items-center justify-end gap-4 border border-t-0 border-stone-200 px-6 py-4 text-sm dark:border-stone-700"
             :class="{
                 'rounded-b': size === 'sm' || size === 'md',
                 'rounded-b-lg': size === 'lg',
